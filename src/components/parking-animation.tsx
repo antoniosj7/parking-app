cb506badec3dc5096a354d6117268c70cf44d949
Ant Dragnil <antoniosamayoa5@gmail.com>
2025-10-21 07:32:50 +0000

    cambia completamente la animacion no se ve bien utiliza solo una figura

--- src/components/parking-animation.tsx
+++ src/components/parking-animation.tsx
@@ -2,17 +2,15 @@
 import { cn } from "@/lib/utils";
 import styles from "./parking-animation.module.css";
 
-// Un SVG de coche con estilo de contorno, inspirado en la imagen de referencia.
+// Un SVG de coche con una forma más definida y estilizada
 const CarSvg = ({ className }: { className?: string }) => (
   <svg
     className={cn(styles.car, className)}
-    viewBox="0 0 200 75"
+    viewBox="0 0 70 40" // Ajustamos el viewBox para la nueva forma
     preserveAspectRatio="xMidYMid meet"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      d="M10.5,42.5 C1,38 1,28 10,23 L35,8 C40,5 55,5 60,8 L105,8 C110,5 125,5 130,8 L170,23 C180,28 180,38 170,42.5 L158,50 L158,60 C158,65 154,70 148,70 L130,70 C125,70 120,65 120,60 L120,58 L60,58 L60,60 C60,65 55,70 50,70 L32,70 C26,70 22,65 22,60 L22,50 L10.5,42.5 Z"
-    />
+    <path d="M5 20 C0 20, 0 10, 10 10 L20 5 C25 2, 45 2, 50 5 L60 10 C70 10, 70 20, 65 20 L63 22 C60 25, 60 30, 63 32 L65 35 C70 35, 70 38, 65 38 H5 C0 38, 0 35, 5 35 L7 32 C10 30, 10 25, 7 22 L5 20 Z" />
   </svg>
 );
 
